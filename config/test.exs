f78b817a78849e2b1f235701eb88a5f36f56af90
Zack Kollar <rallokkcaz@gmail.com>
2019-07-14 03:31:17 -0700

    Integration tests

--- config/test.exs
+++ config/test.exs
@@ -1,5 +1,7 @@
 use Mix.Config
 
+config :logger, level: :error
+
 # Set simple/sane defaults for local testing and CI.
 config :access_pass, :ecto_repos, [AccessPass.Repo]
 config :access_pass, AccessPass.Repo,
@@ -10,7 +12,9 @@ config :access_pass, AccessPass.Repo,
   pool: Ecto.Adapters.SQL.Sandbox,
   priv: "test/"
 config :access_pass, AccessPass.Mailer,
-  adapter: Bamboo.LocalAdapter
+  adapter: Bamboo.TestAdapter
+config :access_pass,
+  from: "info@test-email.com"
 
 # Set arbitrary expiration to pass tests
 config :access_pass, :access_expire_in, 5
--- test/gate_keeper_test.exs
+++ test/gate_keeper_test.exs
@@ -4,16 +4,55 @@ defmodule GateKeeperTest do
 
   alias AccessPass.GateKeeper
 
+  @valid_user_obj %{
+    username: "example",
+    password: "password123",
+    password_confirm: "password123",
+    email: "example@email.com"
+  }
+
+  setup do
+    GateKeeper.register(@valid_user_obj)
+  end
+
   test "can register user" do
     {result, token} = GateKeeper.register(%{
-      username: "example",
-      password: "otherexample",
-      password_confirm: "otherexample",
-      email: "example@email.com"
+      username: "example2",
+      password: "password123",
+      password_confirm: "password123",
+      email: "example2@email.com"
     })
 
     assert result == :ok
     assert token[:type] == "basic"
     assert token[:access_expire_in] == 5
+  end
+
+  test "cannot register invalid user" do
+    {result, _token} = GateKeeper.register(%{
+      username: "example2",
+      password: "password123",
+      password_confirm: "password123",
+    })
+
+    assert result == :error
+  end
+
+  test "cannot register user that already exists" do
+    {result, _token} = GateKeeper.register(@valid_user_obj)
+    assert result == :error
+  end
+
+  test "can login user" do
+    {result, token} = GateKeeper.log_in("example", "password123")
+
+    assert result == :ok
+    assert token[:type] == "basic"
+    assert token[:access_expire_in] == 5
+  end
+
+  test "cannot login non-existant user" do
+    {result, _token} = GateKeeper.log_in("idonotexist", "fakepassword")
+    assert result == :error
   end
 end
